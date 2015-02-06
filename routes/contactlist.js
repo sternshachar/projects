12ce1ec64d0b1968eb77feb8717de69cc10148a5
Stern <stern@Sterns-MacBook-Pro.local>
2015-02-06 13:54:39 +0200

    Adding routes

---
+++ routes/contactlist.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser());
+
+router.route('/')
+	.get(function(request,response){
+		Person.find(function(err,persons){
+			if(err) return console.error(err);
+			console.log(persons);
+			response.json(persons);
+		});
+	})
+	.post(function(request,response){
+	var newContact = request.body;
+	var newPerson = new Person(newContact);
+	newPerson.save(function(err, newPerson){
+			if(err) return console.error(err);
+			console.log('New contact added!');
+			console.log(newPerson);
+		})
+	response.status(201).json(newPerson);
+	});
+
+router.route("/:id")
+	.put(function(request,response){
+		var id = request.params.id;
+		var updatedPerson = request.body;
+		Person.update({ _id: id },{ $set: updatedPerson },function(){
+			Person.findById(id, function(err,person){
+				if(err) return console.error(err);
+				console.log(person);
+				response.json(person);
+			});
+		});
+		
+	})
+
+	.delete(function(request,response){
+		var id = request.params.id;
+		Person.find({ _id : id}).remove().exec();
+		response.json(id);
+	});
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* --- Setting up an express server --- */
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser());
+var contactlist = require('./routes/contactlist');
+app.use('/contactlist', contactlist);
 app.use(express.static('public'));
 app.listen(8080);
 /* --- End of express server setup --- */
@@ -20,7 +20,6 @@ var person = [];
 
 db.once('open', function(callback){
 	console.log('Connected to mongoDB!');
-
 /* --- Setting contactlist collection with Schema --- */
 	 personsSchema = mongoose.Schema({ name: String, email: String, number: String},{collection: 'contactlist'});
 	 Person = mongoose.model('contactlist',personsSchema);
@@ -28,43 +27,4 @@ db.once('open', function(callback){
 });
 /* --- End of mongoose connection setup --- */
 
-
-
-app.get('/contactlist', function(request,response){
-	Person.find(function(err,persons){
-		if(err) return console.error(err);
-		console.log(persons);
-		response.json(persons);
-	});
-});
-
-app.post('/contactlist', function(request,response){
-	var newContact = request.body;
-	var newPerson = new Person(newContact);
-	newPerson.save(function(err, newPerson){
-		if(err) return console.error(err);
-		console.log('New contact added!');
-		console.log(newPerson);
-	})
-	response.status(201).json(newPerson);
-});
-
-app.put('/contactlist/:id', function(request,response){
-	var id = request.params.id;
-	var updatedPerson = request.body;
-	Person.update({ _id: id },{ $set: updatedPerson },function(){
-		Person.findById(id, function(err,person){
-			if(err) return console.error(err);
-			console.log(person);
-			response.json(person);
-		});
-	});
-	
-});
-
-app.delete('/contactlist/:id', function(request,response){
-	var id = request.params.id;
-	Person.find({ _id : id}).remove().exec();
-	response.json(id);
-});
 //comment to text nodemon
