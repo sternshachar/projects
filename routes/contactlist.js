7213fee558dc5abedbaeacd9abd2fbd1d0db3362
Stern <stern@Sterns-MBP.Home>
2015-02-08 18:13:29 +0200

    seperating authentication

--- routes/contactlist.js
+++ routes/contactlist.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var passport = require('./routes/authenticate');
+var passport = require('./authenticate');
 
 
 router.use(bodyParser());
