15368dc3f56cb9d2f35a960d9273feef14e4e303
Stern <stern@Sterns-MBP.Home>
2015-02-08 17:20:12 +0200

    authenticate with database

--- routes/contactlist.js
+++ routes/contactlist.js
@@ -21,7 +21,7 @@ passport.use(new passportLocal(function(username,password,done){
 	console.log(username + password);
 	User.find({"name": username },function(err,user){
 		if(err) return console.error(err);
-		console.log(user);
+		console.log(user.password + password);
 		if(user.password == password){
 			console.log('found');
 			done(null,{id: user._id ,name: username});
